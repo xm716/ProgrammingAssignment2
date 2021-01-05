209506dba129e8a6e57aa87f7326ace1575d538e
xm716 <77001987+xm716@users.noreply.github.com>
2021-01-05 19:19:30 +0800

    Add files via upload

---
+++ makecachematrix.R
@@ -0,0 +1,26 @@
+makeCacheMatrix<-function(x=matrix()){
+  inv <- NULL
+  set <- function(y) {
+    #the single arrow operator always work at the current level
+    #the double arrow operator is capable of modifying variables in the parent level
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){x}
+  setinverse <- function(inverse){inv<<-inverse}
+  getinverse <-function(){inv}
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+}
+
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setinverse(inv)
+  inv
+}
